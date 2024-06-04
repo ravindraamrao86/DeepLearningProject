71b22d47b1e3f7d7bd4b47962ea58a77aecdba47
ravindraamrao1986 <ravindra.amrao1986@gmail.com>
2024-06-04 12:02:34 +0530

    data_ingestion.py

---
+++ Xray/cloud_storage/s3_operation.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from Xray.exception import XRayException # type: ignore
+
+class s3Operation:
+    def sync_folder_to_s3()-> None:
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e,sys)
+        
+        
+        
+    def sync_sync_folder_to_s3()-> None:
+        try:
+            pass
+        except Exception as e:
+        
+         raise XRayException(e,sys)  
